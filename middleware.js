7e58e46596934464382a9789bb5b206fb6d1e0bd
HKinnovaTech <tech.hamzakhan@gmail.com>
2025-02-25 00:45:42 +0500

    middleware

--- middleware.js
+++ middleware.js
@@ -1,14 +1,15 @@
-import { next } from '@vercel/edge';
+import { NextResponse } from 'next/server';
 
-export default function middleware(req) {
-  return next({
-    headers: {
-      'Referrer-Policy': 'origin-when-cross-origin',
-      'X-Frame-Options': 'DENY',
-      'X-Content-Type-Options': 'nosniff',
-      'X-DNS-Prefetch-Control': 'on',
-      'Strict-Transport-Security':
-        'max-age=31536000; includeSubDomains; preload',
-    },
-  });
+export function middleware(req) {
+  const headers = new Headers(req.headers);
+  headers.set('Referrer-Policy', 'origin-when-cross-origin');
+  headers.set('X-Frame-Options', 'DENY');
+  headers.set('X-Content-Type-Options', 'nosniff');
+  headers.set('X-DNS-Prefetch-Control', 'on');
+  headers.set(
+    'Strict-Transport-Security',
+    'max-age=31536000; includeSubDomains; preload'
+  );
+
+  return NextResponse.next({ headers });
 }
